574f54571ae87c2617fcea9b512a3a6a2d152241
a4dydx <95072071+a4dydx@users.noreply.github.com>
2022-01-17 17:14:40 +0300

    made this exist
    Coded index.html
    Added button through app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button');
+
+btn.onclick = () =>{
+    alert("Thank you for clicking me")
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
